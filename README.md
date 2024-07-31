d36b11bfdd2e5edc2b9c42a62eed38bbf62829cc
Benn1440 <benokafor67@gmail.com>
2024-07-31 12:23:51 +0100

    Created basic app

---
+++ README.md
@@ -0,0 +1,3 @@
+# My App
+
+This is a simple Python application that displays a welcome message.
---
+++ app/greet.py
@@ -0,0 +1,3 @@
+def greet():
+    message = "Hello, Welcome to KodeCamp DevOps Bookcamp!"
+    print(message)
