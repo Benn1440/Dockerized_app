ce292dc31d23ae99d5a5b4f52784d792ec6044e9
Benn1440 <benokafor67@gmail.com>
2024-08-03 07:51:33 +0100

    integrated configMap response into app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Ben, Welcome to KodeCamp DevOps Bootcamp!"
+    # return "Hello Ben, Welcome to KodeCamp DevOps Bootcamp!"
+     message = os.getenv('Welcome_Message', 'Hello, Welcome to KodeCamp DevOps Bootcamp!')
+     return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5500)
